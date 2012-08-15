b1031adfb220006ab6f6b4c3ae8114c6d1ffce17
richardl <richardlama@gmail.com>
2012-08-16 06:41:47 +1000

    credential file changed

--- credentials.js
+++ credentials.js
@@ -1,8 +1,8 @@
 var credentials = {
-  consumer_key: 'zXGSkyNvwbmmE80rLloz6Q',
-  consumer_secret: 'YMjkzFmFjQWOGC42G2oFRN58UPvp3BS2vNdCJ1tIvJ4',
-  access_token_key: '137680873-bZiPYEhyQGUiA8NMviB4RFzeWznPyw53E2Z8Av6U',
-  access_token_secret: 'RLBW4vOvSaSfdSiJd5SUGHIUnMgIYZiOuZaHEWgznU8'
+  consumer_key: 'consumer_key',
+  consumer_secret: 'consumer_secret',
+  access_token_key: 'access_token_key',
+  access_token_secret: 'access_token_secret'
 };
 
 
